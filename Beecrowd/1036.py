930dc31c85a354c72269b1f9fccba47df5eebcbf
Tawfiqul Hasan <tawfiqul.abid@gmail.com>
2026-05-13 12:05:54 +0600

    1036

---
+++ Beecrowd/1036.py
@@ -0,0 +1,10 @@
+import math
+a, b, c = map(float, input().split())
+s = (b*b)-4*a*c
+if s > 0 and a > 0:
+    r1 =  (-b + math.sqrt(s))/(2*a)
+    r2 =  (-b - math.sqrt(s))/(2*a)
+    print(f"R1 = {r1:.5f}\nR2 = {r2:.5f}")
+
+else:
+    print("Impossivel calcular")
