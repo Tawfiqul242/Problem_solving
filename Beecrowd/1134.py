c40e3c3e3506dc86920c8a919761c6bd97c93240
Tawfiqul Hasan <tawfiqul.abid@gmail.com>
2026-08-05 20:50:18 +0600

    1134

---
+++ Beecrowd/1134.py
@@ -0,0 +1,16 @@
+a = g = d = 0
+while(True):
+    n = int(input())
+    if n == 1:
+        a += 1
+    elif n == 2:
+        g+=1
+    elif n == 3:
+        d+=1
+    elif n == 4:
+        break
+
+print(f"MUITO OBRIGADO")
+print(f"Alcool: {a}")
+print(f"Gasolina: {g}")
+print(f"Diesel: {d}")
