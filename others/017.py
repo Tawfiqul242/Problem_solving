9a6ba64ef4eb68be787a167538c1544b5980043f
Tawfiqul Hasan <tawfiqul.abid@gmail.com>
2026-08-25 01:19:21 +0600

    upper() lower()

---
+++ others/017.py
@@ -0,0 +1,12 @@
+# Capitalize the first letter of every word
+
+sentence = input()
+print(sentence.title())
+
+print(sentence.capitalize())
+
+print(sentence.upper())
+
+print(sentence.lower())
+
+print(sentence.islower())
